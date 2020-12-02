688a8f36193e9bc3e377bd006535b4d4593a4044
otaviobambans <otavio21072002@gmail.com>
2020-12-01 21:21:14 -0300

    Last JS version - 1.2
    All running

--- cep_script.js
+++ cep_script.js
@@ -1,72 +1,44 @@
 const inputs = document.querySelectorAll('.input_response')
-const form_action = ()=>{
-	var cep = document.querySelector('#input_cep').value
-	if(!isNaN(cep)){
-		if(cep.length == 8){
-			via_cep(cep)
-		}
-		else if(cep.length == 0){
-			form_clear()
-		}
-	}
-	else{
-		document.querySelector('#input_cep').value = null
-	}
-}
+const cep = document.querySelector('#input_cep')
 const via_cep = cep_value=>{
 	const link_to_fetch = `https://viacep.com.br/ws/${cep_value}/json/`
-	document.querySelector('a').setAttribute('href', link_to_fetch)
-	document.querySelector('#link').value = link_to_fetch
-	fecthing(link_to_fetch)
-}
-const fecthing = link_to_fetch =>{
 	fetch(link_to_fetch)
 		.then(data => data.json())
 		.then(response=>{
-			html_cep(response)
+			document.querySelector('a').setAttribute('href', link_to_fetch)
+			document.querySelector('#link').value = link_to_fetch
+			put_in_html(response)
 		})
 		.catch(error=>{
-			console.table(error)
+			error ? form_clear() : false
 		})
 }
-const html_cep = response=>{
-	var responses = [response.cep, response.logradouro, response.complemento, response.bairro, response.localidade, response.uf, response.ibge, response.gia, response.ddd, response.siafi]
-	for_propagation(responses, inputs, responses, true)
+const put_in_html = response=>{
+	if(response.erro){
+		window.location.reload()
+	}
+	else{
+		var responses = [response.cep, response.logradouro, response.complemento, response.bairro, response.localidade, response.uf, response.ibge, response.gia, response.ddd, response.siafi]
+		for_propagation(responses, inputs, responses, true)
+	}
 }
 const form_clear = ()=>{
 	for_propagation(inputs, inputs, null, false)
-	document.querySelector('#input_cep').value = null
+	cep.value = null
 	document.querySelector('a').setAttribute('href', 'https://viacep.com.br/')
 	document.querySelector('#link').value = 'https://viacep.com.br/'
 }
 const for_propagation = (for_array, array_value_recieve, values, values_in_loop)=>{
-	if(values_in_loop){
-		for(i=0; i<=for_array.length - 1; i++){
-			array_value_recieve[i].value = values[i]
-		}
-	}
-	else{
-		for(i=0; i<=for_array.length - 1; i++){
-			array_value_recieve[i].value = values
-		}
+	values_in_loop ? result_values = 'values[i]' : result_values = 'values'
+	for(i=0; i<=for_array.length - 1; i++){
+		array_value_recieve[i].value = eval(result_values)
 	}
 }
-const keyboard_check = () => {
-	document.querySelector('#input_cep').addEventListener('keyup', event=>{
-		var key = event.which || event.keyCode || event.key;
-		var reg = new RegExp('/[0-9]/')
-		if(key == reg){
-			form_clear()
-		}
-		else if(key == 27){
-			form_clear()
-		}
-		else{
-			form_action()
-		}
-	})
-}
-const init = ()=>{
-	keyboard_check()
+const keyboard_init = () => cep.addEventListener('keyup', event=>{
+		event.key.match(/\d/g) ? (cep.value.length == 8 ? via_cep(cep.value) : null) : form_clear()
+})
+const init = () => {
+	keyboard_init()
+	cep.focus()
 }
 window.onload = init()
