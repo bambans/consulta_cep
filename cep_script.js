68d9822be041646a7b7bf044dd0eff44592cb231
otaviobambans <otavio21072002@gmail.com>
2020-12-02 00:00:44 -0300

    Last Commit - 2.0
    Consulta Cep - ViaCEP (final)

--- cep_script.js
+++ cep_script.js
@@ -1,10 +1,24 @@
 const inputs = document.querySelectorAll('.input_response')
 const cep = document.querySelector('#input_cep')
+const init = () => {
+	cep_validation()
+	cep.focus()
+}
+const cep_validation = () => cep.addEventListener('keyup', event=>{
+	var cep_valid = cep.value.replace(/\D/g, '')
+	if(cep.length == 0 || cep_valid.length == 0){
+		form_clear()
+	}
+	else if(cep_valid.length == 8){
+		via_cep(cep_valid)
+	}
+})
 const via_cep = cep_value=>{
 	const link_to_fetch = `https://viacep.com.br/ws/${cep_value}/json/`
 	fetch(link_to_fetch)
 		.then(data => data.json())
 		.then(response=>{
+			cep.value = response.cep
 			document.querySelector('a').setAttribute('href', link_to_fetch)
 			document.querySelector('#link').value = link_to_fetch
 			put_in_html(response)
@@ -22,23 +36,16 @@ const put_in_html = response=>{
 		for_propagation(responses, inputs, responses, true)
 	}
 }
-const form_clear = ()=>{
-	for_propagation(inputs, inputs, null, false)
-	cep.value = null
-	document.querySelector('a').setAttribute('href', 'https://viacep.com.br/')
-	document.querySelector('#link').value = 'https://viacep.com.br/'
-}
 const for_propagation = (for_array, array_value_recieve, values, values_in_loop)=>{
 	values_in_loop ? result_values = 'values[i]' : result_values = 'values'
 	for(i=0; i<=for_array.length - 1; i++){
 		array_value_recieve[i].value = eval(result_values)
 	}
 }
-const keyboard_init = () => cep.addEventListener('keyup', event=>{
-		event.key.match(/\d/g) ? (cep.value.length == 8 ? via_cep(cep.value) : null) : form_clear()
-})
-const init = () => {
-	keyboard_init()
-	cep.focus()
+const form_clear = ()=>{
+	for_propagation(inputs, inputs, null, false)
+	cep.value = null
+	document.querySelector('a').setAttribute('href', 'https://viacep.com.br/')
+	document.querySelector('#link').value = 'https://viacep.com.br/'
 }
 window.onload = init()
